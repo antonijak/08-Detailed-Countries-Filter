a45457abdc1466f31e4f2c734c8a2ba11c360875
Antonija <antonija.kasum@gmail.com>
2018-09-25 21:19:06 +0300

    Buttons, sorting, style

--- script.js
+++ script.js
@@ -8,13 +8,11 @@ fetch(url)
     let total = document.querySelector('#total');
     let number = document.querySelector('#number');
     let expression = document.querySelector('#expression');
+    let countrie_s = document.querySelector('#countrie-s');
 
     //when user types, matching coutries are generated
     const searchField = document.querySelector('#search');
     searchField.addEventListener('input', searchByAny)
-    //when user clicks search by any word, matching coutries are generated
-    const searchAnyBtn = document.querySelector('#any-word');
-    searchAnyBtn.addEventListener('click', searchByAny);
 
     // makes an array with objects containing data that we want
     for (let i = 0; i < countriz.length; i++) {
@@ -77,7 +75,9 @@ fetch(url)
       area.textContent = country.area;
       region.textContent = country.region;
       language.textContent = country.language;
-      if (language.textContent.length > 50) {
+      if (language.textContent.length > 70) {
+        language.style.fontSize = '.6rem';
+      } else if (language.textContent.length > 50) {
         language.style.fontSize = '.7rem';
       } else if (language.textContent.length > 40) {
         language.style.fontSize = '.8rem';
@@ -93,21 +93,18 @@ fetch(url)
         currencies.style.fontSize = '.9rem';
       }
       population.textContent = country.population;
-      population.style.textAlign = 'right';
       if (country.population > 100000000) {
         population.style.color = 'red';
       }
       population.style.fontWeight = 'bold';
       flag.src = country.flag;
       flag.className = 'flag';
-      flag.style.width = '17%'
+      flag.style.height = '90%';
     }
 
     function searchByAny() {
       main.innerHTML = '';
       total.textContent = '';
-      searchAnyBtn.className = 'isClicked';
-      searchAnyBtn.className = 'buttons';
       let userInputUpper = document.querySelector('#search').value.toUpperCase();
       let result = countries.filter((countryObj) => {
         for (let key in countryObj) {
@@ -117,14 +114,138 @@ fetch(url)
         }
       })
 
-      function sortByName(){ result.sort( (a, b) => a.name.localeCompare(b.name) )}
-      function sortByCapital(){ result.sort( (a, b) => a.capital.localeCompare(b.capital) )}
-      function sortByRegion(){ result.sort( (a, b) => a.region.localeCompare(b.region) )}
-      function sortByPopulation(){ result.sort( (a, b) => a.population - b.population )}
-      function sortByArea(){ result.sort( (a, b) => a.area - b.area )}
-      sortByArea()
       result.forEach(country => createCountryDiv(country))
-      number.textContent = result.length
+      function grammar (){
+        lengthx = result.length
+        number.textContent = lengthx
+        if (lengthx > 1 || lengthx === 0){
+          verb.textContent = 'are'
+          countrie_s.textContent = 'countries'
+        } else {
+          verb.textContent = 'is'
+          countrie_s.textContent = 'country'
+        }
+      }
+      grammar()
       expression.textContent = userInputUpper;
+
+      const byName = document.querySelector("#legend_country");
+      byName.addEventListener('click', sortByNameAsc);
+      const byCapital = document.querySelector("#legend_capital");
+      byCapital.addEventListener('click', sortByCapitalAsc);
+      const byRegion = document.querySelector("#legend_region");
+      byRegion.addEventListener('click', sortByRegionAsc);
+      const byPopulation = document.querySelector("#legend_population");
+      byPopulation.addEventListener('click', sortByPopulationAsc);
+      const byArea = document.querySelector("#legend_area");
+      byArea.addEventListener('click', sortByAreaAsc);
+
+    
+
+      function sortByNameAsc() {
+        const icon = document.querySelector('#country');
+        icon.src = './assets/up.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => a.name.localeCompare(b.name));
+        result.forEach(country => createCountryDiv(country));
+        byName.removeEventListener('click', sortByNameAsc);
+        byName.addEventListener('click', sortByNameDesc);
+      }
+
+      function sortByCapitalAsc() {
+        const icon = document.querySelector('#capital');
+        icon.src = './assets/up.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => a.capital.localeCompare(b.capital));
+        result.forEach(country => createCountryDiv(country));
+        byCapital.removeEventListener('click', sortByCapitalAsc);
+        byCapital.addEventListener('click', sortByCapitalDesc);
+       
+      }
+
+      function sortByRegionAsc() {
+        const icon = document.querySelector('#region');
+        icon.src = './assets/up.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => a.region.localeCompare(b.region));
+        result.forEach(country => createCountryDiv(country));
+        byRegion.removeEventListener('click', sortByRegionAsc);
+        byRegion.addEventListener('click', sortByRegionDesc);
+      }
+
+      function sortByPopulationAsc() {
+        const icon = document.querySelector('#population');
+        icon.src = './assets/up.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => a.population - b.population);
+        result.forEach(country => createCountryDiv(country));
+        byPopulation.removeEventListener('click', sortByPopulationAsc);
+        byPopulation.addEventListener('click', sortByPopulationDesc);
+      }
+
+      function sortByAreaAsc() {
+        const icon = document.querySelector('#area');
+        icon.src = './assets/up.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => a.area - b.area);
+        result.forEach(country => createCountryDiv(country));
+        byArea.removeEventListener('click', sortByAreaAsc);
+        byArea.addEventListener('click', sortByAreaDesc);
+      }
+
+      //sorting in descending order
+
+      function sortByNameDesc() {
+        const icon = document.querySelector('#country');
+        icon.src = './assets/down.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => b.name.localeCompare(a.name));
+        result.forEach(country => createCountryDiv(country));
+        byName.removeEventListener('click', sortByNameDesc);
+        byName.addEventListener('click', sortByNameAsc);
+      }
+
+      function sortByCapitalDesc() {
+        const icon = document.querySelector('#capital');
+        icon.src = './assets/down.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => b.capital.localeCompare(a.capital));
+        result.forEach(country => createCountryDiv(country));
+        byCapital.removeEventListener('click', sortByCapitalDesc);
+        byCapital.addEventListener('click', sortByCapitalAsc);
+      }
+
+      function sortByRegionDesc() {
+        const icon = document.querySelector('#region');
+        icon.src = './assets/down.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => b.region.localeCompare(a.region));
+        result.forEach(country => createCountryDiv(country));
+        byRegion.removeEventListener('click', sortByRegionDesc);
+        byRegion.addEventListener('click', sortByRegionAsc);
+      }
+
+      function sortByPopulationDesc() {
+        const icon = document.querySelector('#population');
+        icon.src = './assets/down.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => b.population - a.population);
+        result.forEach(country => createCountryDiv(country));
+        byPopulation.removeEventListener('click', sortByPopulationDesc);
+        byPopulation.addEventListener('click', sortByPopulationAsc);
+      }
+
+      function sortByAreaDesc() {
+        const icon = document.querySelector('#area');
+        icon.src = './assets/down.svg';
+        main.innerHTML = '';
+        result.sort((a, b) => b.area - a.area);
+        result.forEach(country => createCountryDiv(country));
+        byArea.removeEventListener('click', sortByAreaDesc);
+        byArea.addEventListener('click', sortByAreaAsc);
+      }
+      
     }
+    searchByAny() 
+    
   })
